b5a7949073f55add00ce9ac33ef1095210689218
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2019-04-12 16:53:58 +0900

    Initial Commit

---
+++ LabLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabLog
+//
+//  Created by 大林拓実 on 2019/04/12.
+//  Copyright © 2019年 TakumiObayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
